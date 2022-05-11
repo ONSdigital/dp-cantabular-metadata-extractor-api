9787816f129e6893e0a468fa219262bc96fe6726
Steve Mynott <steve.mynott@gmail.com>
2022-05-11 11:09:44 +0100

    Cantabular Ext Api config

--- config/config.go
+++ config/config.go
@@ -12,6 +12,7 @@ type Config struct {
 	GracefulShutdownTimeout    time.Duration `envconfig:"GRACEFUL_SHUTDOWN_TIMEOUT"`
 	HealthCheckInterval        time.Duration `envconfig:"HEALTHCHECK_INTERVAL"`
 	HealthCheckCriticalTimeout time.Duration `envconfig:"HEALTHCHECK_CRITICAL_TIMEOUT"`
+	CantabularExtURL           string        `envconfig:"CANTABULAR_EXT_API_URL"`
 }
 
 var cfg *Config
@@ -28,6 +29,7 @@ func Get() (*Config, error) {
 		GracefulShutdownTimeout:    5 * time.Second,
 		HealthCheckInterval:        30 * time.Second,
 		HealthCheckCriticalTimeout: 90 * time.Second,
+		CantabularExtURL:           "http://localhost:8492",
 	}
 
 	return cfg, envconfig.Process("", cfg)
