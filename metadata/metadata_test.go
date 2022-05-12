5938517ad098a835fe935539114c1254a3d9ef3f
Steve Mynott <steve.mynott@gmail.com>
2022-05-12 16:40:11 +0100

    disable integration tests by default. use make test-int

--- metadata/metadata_test.go
+++ metadata/metadata_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -15,10 +16,12 @@ import (
 	"github.com/ONSdigital/dp-api-clients-go/v2/cantabular"
 	"github.com/ONSdigital/dp-api-clients-go/v2/cantabular/mock"
 
-	//dphttp "github.com/ONSdigital/dp-net/http"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+var intFlag = flag.Bool("int", false, "perform int tests")
+
+// this probably belongs in dp-api-clients-go but is here as a stopgap
 func TestMockGetCantabularMetaDataHappy(t *testing.T) {
 
 	Convey("Given a correct response from the Metadata Server", t, func() {
@@ -70,6 +73,11 @@ func Response(body []byte, statusCode int) *http.Response {
 }
 
 func TestGetCantabularMetaData(t *testing.T) {
+
+	if !*intFlag {
+		t.Skip("not doing int tests")
+	}
+
 	dims := []string{"Age", "Country"}
 
 	resp := getCantabularMetaData("Teaching-Dataset", dims)
@@ -90,17 +98,14 @@ func TestGetCantabularMetaData(t *testing.T) {
 		t.Error("didn't get the same dims back as we sent!")
 	}
 
-	/*
-		// debugging
-		bs, err := json.Marshal(resp)
+	// debugging
+	bs, err := json.Marshal(resp)
 
-		if err != nil {
-			t.Fail()
-		}
+	if err != nil {
+		t.Fail()
+	}
 
-		println(jsonpp(bs))
-
-	*/
+	println(jsonpp(bs))
 
 }
 
