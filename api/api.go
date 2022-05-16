4eec120bcbcfbff7ffb6d4d41c6b55314123c7fe
Valentina <vmilitaru28@gmail.com>
2022-05-16 14:54:18 +0100

    adds the config to the CantabularMetadataExtractorAPI struct

--- api/api.go
+++ api/api.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/ONSdigital/dp-api-clients-go/v2/dataset"
 	"github.com/gorilla/mux"
+	"github.com/ONSdigital/dp-cantabular-metadata-extractor-api/config"
 )
 
 // DatasetAPI - An interface used to access the DatasetAPI
@@ -14,13 +15,15 @@ type DatasetAPI interface {
 type CantabularMetadataExtractorAPI struct {
 	Router *mux.Router
 	datasetAPI	DatasetAPI
+	cfg  *config.Config
 }
 
 //Setup function sets up the api and returns an api
-func Setup(ctx context.Context, r *mux.Router, d DatasetAPI) *CantabularMetadataExtractorAPI {
+func Setup(ctx context.Context, r *mux.Router, config  *config.Config, d DatasetAPI) *CantabularMetadataExtractorAPI {
 	api := &CantabularMetadataExtractorAPI{
 		Router: r,
 		datasetAPI: d,
+		cfg:config,
 	}
 
 	r.HandleFunc("/metadata/datasets/{datasetID}/editions/{editionID}/versions/{versionID}", api.getMetadata).Methods("GET")
