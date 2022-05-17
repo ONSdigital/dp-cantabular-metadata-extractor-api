7fcd6ec9ce6dc649c8b51ec8a261bd6caa4723dc
Steve Mynott <steve.mynott@gmail.com>
2022-05-17 14:06:21 +0100

    generate mock/datasetapi.go from 'make generate'

--- api/api.go
+++ api/api.go
@@ -4,26 +4,28 @@ import (
 	"context"
 
 	"github.com/ONSdigital/dp-api-clients-go/v2/dataset"
-	"github.com/gorilla/mux"
 	"github.com/ONSdigital/dp-cantabular-metadata-extractor-api/config"
+	"github.com/gorilla/mux"
 )
+
+//go:generate moq -out mock/datasetapi.go -pkg mock . DatasetAPI
 
 // DatasetAPI - An interface used to access the DatasetAPI
 type DatasetAPI interface {
 	GetVersionDimensions(ctx context.Context, userAuthToken, serviceAuthToken, collectionID, id, edition, version string) (m dataset.VersionDimensions, err error)
 }
 type CantabularMetadataExtractorAPI struct {
-	Router *mux.Router
-	datasetAPI	DatasetAPI
-	cfg  *config.Config
+	Router     *mux.Router
+	datasetAPI DatasetAPI
+	cfg        *config.Config
 }
 
 //Setup function sets up the api and returns an api
-func Setup(ctx context.Context, r *mux.Router, config  *config.Config, d DatasetAPI) *CantabularMetadataExtractorAPI {
+func Setup(ctx context.Context, r *mux.Router, config *config.Config, d DatasetAPI) *CantabularMetadataExtractorAPI {
 	api := &CantabularMetadataExtractorAPI{
-		Router: r,
+		Router:     r,
 		datasetAPI: d,
-		cfg:config,
+		cfg:        config,
 	}
 
 	r.HandleFunc("/metadata/datasets/{datasetID}/editions/{editionID}/versions/{versionID}", api.getMetadata).Methods("GET")
