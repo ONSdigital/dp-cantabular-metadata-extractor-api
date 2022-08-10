3936f813a0e43c7593d53fa1d1644f0664f90cf1
Steve Mynott <steve.mynott@gmail.com>
2022-08-10 11:11:43 +0100

    bump version for self

--- devstack/scs-md.sh
+++ devstack/scs-md.sh
@@ -17,7 +17,7 @@ SERVICES=(
     "dp-api-router,479c4c05d9d506993915d6cee0af356f7ed525be|"
     "dp-cantabular-api-ext,7dafd3c8cd8832b5644008a8248d57e5ee2924dc|make setup"
     "dp-cantabular-dimension-api,2872fdc4234d953ec050be6dc4b595c0a16eb260|"
-    "dp-cantabular-metadata-extractor-api,d643eb8ffb1ccafb9174cc107590abd36cfe5532|"
+    "dp-cantabular-metadata-extractor-api,d3a786fff20662c3f79e83d013e7bd83bab4f596|"
     "dp-cantabular-metadata-service,684c92c3cd8ed42f5ba2a5da640218de4f5f3ffb|make setup"
     "dp-cantabular-server,36bc476a7279c49efe405b8cf1fec25510f723d4|make setup"
     "dp-compose,2b4b94a7ce8894365d28a13fec315ed0d6a610e7|"
