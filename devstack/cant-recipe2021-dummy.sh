480419cc9227989950457c6172febe9f34f60bf7
Steve Mynott <steve.mynott@gmail.com>
2022-08-15 10:38:24 +0100

    first attempt at dummy data recipe

---
+++ devstack/cant-recipe2021-dummy.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+#set -x
+ 
+# This is RM003 ("Dataset_Mnemonic_2011": "LC4402EW") but with no geography dimension
+# due to a mismatch between the real 2021 metadata & synthetic 2021 data.
+#
+# This should be something like OA or RGNCD(?)
+#
+#           {
+#            "href": "http://localhost:22400/code-lists/RGNCD",
+#            "id": "RGNCD",
+#            "is_hierarchy": false,
+#            "name": "Region",
+#            "is_cantabular_geography": true,
+#            "is_cantabular_default_geography": true
+#          },
+
+url=http://localhost:8082/login
+token=$(curl -s -d "{\"email\":\"florence@magicroundabout.ons.gov.uk\",\"password\":\"$FLORENCE_WEB_PW\"}" $url)
+
+payload='{
+  "alias": "Testing for metadata demo v3",
+  "cantabular_blob": "dummy_data_households",
+  "format": "cantabular_table",
+  "id": "la2e031b-3064-427d-8fed-4b35c99bf1a0",
+  "output_instances": [
+    {
+       
+        "code_lists": [
+          {
+            "href": "http://localhost:22400/code-lists/accommodation_type_5a",
+            "id": "accommodation_type_5a",
+            "is_hierarchy": false,
+            "name": "Accommodation type (5 categories)",
+            "is_cantabular_geography": false,
+             "is_cantabular_default_geography": false
+          },
+          {
+            "href": "http://localhost:22400/code-lists/heating_type_3a",
+            "id": "heating_type_3a",
+            "is_hierarchy": false,
+            "name": "Type of central heating in household (3 categories)",
+            "is_cantabular_geography": false,
+             "is_cantabular_default_geography": false
+          },
+          {
+            "href": "http://localhost:22400/code-lists/hh_tenure_5a",
+            "id": "hh_tenure_5a",
+            "is_hierarchy": false,
+            "name": "Tenure of household (5 categories)",
+            "is_cantabular_geography": false,
+            "is_cantabular_default_geography": false
+          }
+      ],
+      "dataset_id": "RM003",
+      "editions": [
+        "2021"
+      ],
+      "title": "Testing for metadata demo v3"
+    }
+  ]
+}'
+
+curl -X POST http://localhost:22300/recipes -d "$payload" -H "accept: application/json" -H "Content-Type: application/json" -H "X-Florence-Token: $token"
