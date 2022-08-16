445363d9fca802b23fb1b48521fa0d9448d06f3e
Steve Mynott <steve.mynott@gmail.com>
2022-08-16 09:17:39 +0100

    write out logs in directory

--- devstack/alllogs.sh
+++ devstack/alllogs.sh
@@ -4,8 +4,12 @@ LINES="${1:-8}"
 
 conts=($(docker ps --filter=name="cantabular-metadata-pub" --format="{{.Names}}"))
 
+if [[ ! -d logs ]]; then
+    mkdir logs
+fi
+
 for cont in "${conts[@]}"; do
     echo
     echo "$cont"
-    docker logs $cont > $cont.log
+    docker logs $cont > logs/$cont.log
 done
