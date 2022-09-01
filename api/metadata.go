153d2167cea22c05775bb1e2e8288f20024fac17
Steve Mynott <steve.mynott@gmail.com>
2022-09-01 16:00:58 +0100

    hardcode response to be ltla

--- api/metadata.go
+++ api/metadata.go
@@ -37,6 +37,24 @@ func (api *CantabularMetadataExtractorAPI) getMetadata(w http.ResponseWriter, r 
 		return
 	}
 
+	// we always override the geographical code (index 0) to ltla since the
+	// "base" in the metadata is "oa" which isn't suitable - Fran 20220831
+
+	// XXX first attempt for MVP but this should be more robust
+	// TODO query the MD again for ltla content if ltla not in use (usual case)
+	// look for geo variable properly and replace whole var
+
+	mt.Service.Tables[0].Vars[0] = "ltla"
+
+	md.Dataset.Vars[0].Name = "ltla"
+	md.Dataset.Vars[0].Description = "As of 2022 there are 309 lower tier local authorities in England, comprising non-metropolitan districts (181), unitary authorities (59), metropolitan districts (36) and London boroughs (33, including City of London). There are 22 lower tier local authorities in Wales, comprising 22 unitary authorities"
+	md.Dataset.Vars[0].Label = "Lower Tier Local Authorities"
+
+	md.Dataset.Vars[0].Meta.ONSVariable.VariableTitle = "Lower Tier Local Authorities"
+	md.Dataset.Vars[0].Meta.ONSVariable.GeographicTheme = "Administrative"
+	md.Dataset.Vars[0].Meta.ONSVariable.VariableMnemonic = "ltla"
+	md.Dataset.Vars[0].Meta.ONSVariable.VariableTitle = "Lower Tier Local Authorities"
+
 	m := cantabular.MetadataQueryResult{TableQueryResult: mt, DatasetQueryResult: md}
 
 	json, err := json.Marshal(m)
