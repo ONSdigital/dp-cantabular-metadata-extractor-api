83c060d0c012f697e4a07f80d93d66c4f5c75b36
Steve Mynott <steve.mynott@gmail.com>
2022-09-15 16:54:45 +0100

    TS013 now broken with UR. Replace by V's TS015

--- devstack/makerecp/createrecipe/createrecipe.go
+++ devstack/makerecp/createrecipe/createrecipe.go
@@ -175,7 +175,7 @@ func GetMap() map[string][]string {
 		"RM154": {"oa", "welsh_skills_speak", "has_ever_worked"}, // WORKS
 		"TS002": {"oa", "legal_partnership_status"},              // WORKS
 		"TS009": {"ltla", "sex"},                                 // WORKS
-		"TS013": {"oa", "passports_all"},                         // WORKS
+		"TS015": {"oa", "year_arrival_uk"},                       // WORKS
 	}
 
 	// Following should also work
