3398b9b8835ab5e75bc53191bcbebc7d7300be3f
Steve Mynott <steve.mynott@gmail.com>
2022-09-22 14:41:22 +0100

    replace original cant-recipe.sh with new

--- devstack/cant-recipe.sh
+++ devstack/cant-recipe.sh
@@ -1,43 +1,10 @@
 #!/usr/bin/env bash
-
 #set -x
+
+ID=${1:-TS009}
 
 url=http://localhost:8082/login
 token=$(curl -s -d "{\"email\":\"florence@magicroundabout.ons.gov.uk\",\"password\":\"$FLORENCE_WEB_PW\"}" $url)
 
-payload='{
-  "alias": "Testing for metadata demo v3",
-  "cantabular_blob": "UR",
-  "format": "cantabular_table",
-  "id": "la2e031b-3064-427d-8fed-4b35c99bf1a0",
-  "output_instances": [
-  {
-
-    "code_lists": [
-    {
-      "href": "http://localhost:22400/code-lists/ltla",
-      "id": "ltla",
-      "is_hierarchy": false,
-      "name": "ltla",
-      "is_cantabular_geography": true,
-      "is_cantabular_default_geography": true
-    },
-    {
-      "href": "http://localhost:22400/code-lists/sex",
-      "id": "sex",
-      "is_hierarchy": false,
-      "name": "sex",
-      "is_cantabular_geography": false,
-      "is_cantabular_default_geography": false
-    }
-    ],
-    "dataset_id": "TS009",
-    "editions": [
-    "2021"
-      ],
-      "title": "Testing for metadata demo v3"
-    }
-  ]
-}'
-
+payload=$(./makerecp -id $ID)
 curl -X POST http://localhost:22300/recipes -d "$payload" -H "accept: application/json" -H "Content-Type: application/json" -H "X-Florence-Token: $token"
