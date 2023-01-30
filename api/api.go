a059a74135d7c97f5e4384a5a67febbf7b3d885f
Greg Fujar <greg@roundtripper.io>
2023-01-30 18:47:58 +0000

    review changes

--- api/api.go
+++ api/api.go
@@ -3,14 +3,14 @@ package api
 import (
 	"context"
 
-	"github.com/ONSdigital/dp-authorisation/v2/authorisation"
 	"github.com/ONSdigital/dp-api-clients-go/v2/cantabular"
+	"github.com/ONSdigital/dp-authorisation/v2/authorisation"
 	"github.com/ONSdigital/dp-cantabular-metadata-extractor-api/config"
 	"github.com/gorilla/mux"
 )
 
 const (
-	CantabularMetadataDatasetReadPermission   string = "cantabularmetadatadataset:read"
+	CantabularMetadataDatasetReadPermission string = "cantabularmetadatadataset:read"
 )
 
 //go:generate moq -out mock/datasetapi.go -pkg mock . CantMetaAPI
@@ -25,23 +25,23 @@ type CantabularMetadataExtractorAPI struct {
 	Router      *mux.Router
 	CantMetaAPI CantMetaAPI
 	Cfg         *config.Config
-	auth          authorisation.Middleware
+	auth        authorisation.Middleware
 }
 
 // Setup function sets up the api and returns an api
-func Setup(ctx context.Context, 
-	r *mux.Router, 
-	config *config.Config, 
-	c CantMetaAPI, 
+func Setup(ctx context.Context,
+	r *mux.Router,
+	config *config.Config,
+	c CantMetaAPI,
 	auth authorisation.Middleware) *CantabularMetadataExtractorAPI {
 
 	api := &CantabularMetadataExtractorAPI{
 		Router:      r,
 		CantMetaAPI: c,
 		Cfg:         config,
-		auth: auth,
+		auth:        auth,
 	}
 
-	r.HandleFunc("/cantabular-metadata/dataset/{datasetID}/lang/{lang}", auth.Require(CantabularMetadataDatasetReadPermission, api.getMetadata)).Methods("GET")
+	r.HandleFunc("/cantabular-metadata/dataset/{datasetID}/lang/{lang}", auth.Require(CantabularMetadataDatasetReadPermission, api.getMetadataHandler)).Methods("GET")
 	return api
 }
